8a142622fd9a4c299e833ae699b5852f8e93691d
malekabd2019-cell <malekabd2019@gmail.com>
2026-07-24 02:58:06 +0300

    Update tclac.h

--- components/tclac/tclac.h
+++ components/tclac/tclac.h
@@ -16,15 +16,19 @@ class TCLAC : public climate::Climate, public uart::UARTDevice, public Component
   climate::ClimateTraits traits() override;
   void control(const climate::ClimateCall &call) override;
 
-  // دالة استقبال وتثبيت وضع المولد في الذاكرة
+  // دالة ضبط وضع المولد وتثبيته في ذاكرة ESP32 مع الإرسال الفوري
   void set_gen_mode(uint8_t mode) {
     this->gen_mode_ = mode;
     this->send_data();
   }
 
+  uint8_t get_gen_mode() const { return this->gen_mode_; }
+
  private:
   void send_data();
   void read_data();
+  
+  // متغير حفظ وضع المولد الأصلي بداخل الذاكرة
   uint8_t gen_mode_{0x00};
 };
 
