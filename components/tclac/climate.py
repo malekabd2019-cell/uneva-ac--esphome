5761bf079b0ef231d1f7da0abb40c4b99ed3859e
malekabd2019-cell <malekabd2019@gmail.com>
2026-07-24 04:46:52 +0300

    Fix variable creation in climate.py
    Updated variable creation to use new_Pvariable for consistency.

--- components/tclac/climate.py
+++ components/tclac/climate.py
@@ -7,10 +7,8 @@ DEPENDENCIES = ["uart"]
 AUTO_LOAD = ["climate"]
 
 tclac_ns = cg.esphome_ns.namespace("tclac")
-# استخدام اسم الكلاس الصحيح tclacClimate المطابق لملفات C++
 tclacClimate = tclac_ns.class_("tclacClimate", climate.Climate, uart.UARTDevice, cg.PollingComponent)
 
-# دعم التوافق مع إصدارات ESPHome الحديثة والقديمة بدون أخطاء
 if hasattr(climate, "climate_schema"):
     BASE_SCHEMA = climate.climate_schema(tclacClimate)
 else:
@@ -25,7 +23,8 @@ CONFIG_SCHEMA = BASE_SCHEMA.extend(
 ).extend(cv.polling_component_schema("5s")).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_variable(config[CONF_ID])
+    # استخدام new_Pvariable المصححة بدلاً من new_variable
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
     await uart.register_uart_device(var, config)
