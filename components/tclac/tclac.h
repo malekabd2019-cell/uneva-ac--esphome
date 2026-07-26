9e192e0a1d8ad6fc18a244e59ae41f46cc38a708
malekabd2019-cell <malekabd2019@gmail.com>
2026-07-26 03:04:38 +0300

    Update tclac.h

--- components/tclac/tclac.h
+++ components/tclac/tclac.h
@@ -1,10 +1,3 @@
-/**
-* Create by Miguel Ángel López on 20/07/19
-* and modify by xaxexa
-* Refactoring & component making:
-* Соловей с паяльником 15.03.2024
-**/
-
 #ifndef TCL_ESP_TCL_H
 #define TCL_ESP_TCL_H
 
@@ -12,7 +5,6 @@
 #include "esphome/core/defines.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/climate/climate.h"
-#include "esphome/components/select/select.h"
 
 namespace esphome {
 namespace tclac {
@@ -24,22 +16,22 @@ namespace tclac {
 
 #define MODE_AUTO		0b00110101
 #define MODE_COOL		0b00110001
-#define MODE_DRY		0b00110011
+#define MODE_DRY		0b00110013
 #define MODE_FAN_ONLY	0b00110010
 #define MODE_HEAT		0b00110100
 
 #define FAN_SPEED_POS	8
 #define FAN_QUIET_POS	33
 
-#define FAN_AUTO		0b10000000	//auto
-#define FAN_QUIET		0x80		//silent
-#define FAN_LOW			0b10010000	//	|
-#define FAN_MIDDLE		0b11000000	//	||
-#define FAN_MEDIUM		0b10100000	//	|||
-#define FAN_HIGH		0b11010000	//	||||
-#define FAN_FOCUS		0b10110000	//	|||||
-#define FAN_DIFFUSE		0b10000000	//	POWER [7]
-#define FAN_SPEED_MASK	0b11110000	//FAN SPEED MASK
+#define FAN_AUTO		0b10000000
+#define FAN_QUIET		0x80
+#define FAN_LOW			0b10010000
+#define FAN_MIDDLE		0b11000000
+#define FAN_MEDIUM		0b10100000
+#define FAN_HIGH		0b11010000
+#define FAN_FOCUS		0b10110000
+#define FAN_DIFFUSE		0b10000000
+#define FAN_SPEED_MASK	0b11110000
 
 #define SWING_POS			10
 #define SWING_OFF			0b00000000
@@ -87,28 +79,24 @@ class tclacClimate : public climate::Climate, public esphome::uart::UARTDevice, 
 
 	private:
 		byte checksum;
-		// dataTX с управлением состоит из 38 байт
 		byte dataTX[38];
-		// А dataRX по прежнему из 61 байта
 		byte dataRX[61];
-		// Команда запроса состояния
 		byte poll[8] = {0xBB,0x00,0x01,0x04,0x02,0x01,0x00,0xBD};
-		// Инициализация и начальное наполнение переменных состоянй переключателей
-		bool beeper_status_;
-		bool display_status_;
-		bool force_mode_status_;
-		uint8_t switch_preset = 0;
-		bool module_display_status_;
-		uint8_t switch_fan_mode = 0;
-		bool is_call_control = false;
-		uint8_t switch_swing_mode = 0;
-		int target_temperature_set = 0;
-		uint8_t switch_climate_mode = 0;
-		bool allow_take_control = false;
-		
-		esphome::climate::ClimateTraits traits_;
+		bool beeper_status_{false};
+		bool display_status_{true};
+		bool force_mode_status_{false};
+		uint8_t switch_preset{0};
+		bool module_display_status_{false};
+		uint8_t switch_fan_mode{0};
+		bool is_call_control{false};
+		uint8_t switch_swing_mode{0};
+		int target_temperature_set{0};
+		uint8_t switch_climate_mode{0};
+		bool allow_take_control{false};
 		
 	public:
+		uint8_t gen_mode_{0};
+		void set_gen_mode(uint8_t mode) { this->gen_mode_ = mode; }
 
 		tclacClimate() : PollingComponent(5 * 1000) {
 			checksum = 0;
@@ -138,25 +126,19 @@ class tclacClimate : public climate::Climate, public esphome::uart::UARTDevice, 
 		void set_supported_modes(const std::set<esphome::climate::ClimateMode> &modes);
 		void set_supported_fan_modes(const std::set<esphome::climate::ClimateFanMode> &modes);
 		void set_supported_swing_modes(const std::set<esphome::climate::ClimateSwingMode> &modes);
-
-		// --- إضافات وضع المولد (Generator Mode) ---
-		uint8_t gen_mode_{0};
-		esphome::select::Select *gen_select_{nullptr};
-		void set_gen_mode(uint8_t mode) { this->gen_mode_ = mode; }
-		void set_gen_select(esphome::select::Select *s) { this->gen_select_ = s; }
 		
 	protected:
-		GPIOPin *rx_led_pin_;
-		GPIOPin *tx_led_pin_;
+		GPIOPin *rx_led_pin_{nullptr};
+		GPIOPin *tx_led_pin_{nullptr};
 		ClimateTraits traits() override;
 		std::set<ClimateMode> supported_modes_{};
 		std::set<ClimatePreset> supported_presets_{};
-		AirflowVerticalDirection vertical_direction_;
+		AirflowVerticalDirection vertical_direction_{AirflowVerticalDirection::LAST};
 		std::set<ClimateFanMode> supported_fan_modes_{};
-		AirflowHorizontalDirection horizontal_direction_;
-		VerticalSwingDirection vertical_swing_direction_;
+		AirflowHorizontalDirection horizontal_direction_{AirflowHorizontalDirection::LAST};
+		VerticalSwingDirection vertical_swing_direction_{VerticalSwingDirection::UP_DOWN};
 		std::set<ClimateSwingMode> supported_swing_modes_{};
-		HorizontalSwingDirection horizontal_swing_direction_;
+		HorizontalSwingDirection horizontal_swing_direction_{HorizontalSwingDirection::LEFT_RIGHT};
 };
 }
 }
